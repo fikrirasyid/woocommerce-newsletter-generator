04e3129aa35441587e9cec064da8e0b270e2d3ed
Fikri Rasyid <fikrirasyid@gmail.com>
2014-08-27 20:53:09 +0700

    Changing the height of previewing screen
    550 makes more sense. No ugly window scrolling on common 1440 screen

--- js/wc-newsletter-generator-editor.js
+++ js/wc-newsletter-generator-editor.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($){
 	var preview_url = $('.preview.button').attr('href');
 
-	$('#newsletter-preview').html('<iframe src="'+ preview_url +'" width="100%" height="800"></iframe>');
+	$('#newsletter-preview').html('<iframe src="'+ preview_url +'" width="100%" height="550"></iframe>');
 });
